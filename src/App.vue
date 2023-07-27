48233221af1d028d227109172b4c8833eb53f193
thebrokenblow <artkrasov@gmail.com>
2023-07-27 04:01:12 +0300

    Added input component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <headerUberEats/>
+  <header-Uber-Eats />
+  <main class="main-container">
+    <input-Restaurant class="mt-5" />
+  </main>
 </template>
 
 <script>
-import headerUberEats from './components/index-components/header-uber-eats'
+import headerUberEats from "./components/index-components/header-uber-eats";
+import inputRestaurant from "./components/index-components/input-restaurant.vue";
 export default {
   components: {
-    headerUberEats
+    headerUberEats,
+    inputRestaurant,
   },
-}
+};
 </script>
 
-<style src="">
+<style>
+.main-container {
+  margin-left: 60px;
+  margin-right: 60px;
+}
 </style>
