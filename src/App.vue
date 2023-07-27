6103a214055364b45fca8c8a8c23d1c8fb86352e
thebrokenblow <artkrasov@gmail.com>
2023-07-27 03:37:22 +0300

    Added bootsrap and header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <headerUberEats/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import headerUberEats from './components/index-components/header-uber-eats'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    headerUberEats
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="">
 </style>
---
+++ src/components/index-components/header-uber-eats.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <div class="d-flex shadow-sm justify-content-between p-4 header">
+      <div>
+        <a class="logo-header" href="#">
+          <img src="./svg/logo.svg" alt="uber eats" />
+        </a>
+      </div>
+      <div>
+        <button type="button" class="btn btn-outline-dark button-nav">
+          Войти
+        </button>
+        <a class="navbar-brand basket-header" href="#">
+          <img class="basket" src="./svg/basket.svg" alt="basket" />
+        </a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style>
+.header {
+  height: 80px;
+}
+
+.logo-header {
+  padding-left: 40px;
+}
+
+.basket-header {
+  margin-right: 40px;
+}
+
+.basket {
+  height: 30px;
+  width: 30px;
+}
+
+.button-nav {
+  border: 1px solid #e0e0e0;
+  border-radius: 2px;
+  font-weight: 500;
+  margin-right: 20px;
+}
+</style>
